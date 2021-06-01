f1a3b2ba3506e1842326eabf5a7362b0d098af39
KimJiHyun <80038485+zhixxian@users.noreply.github.com>
2021-06-01 14:22:30 +0900

    Delete src directory

--- src/lib/trains/train_factory.py
+++
@@ -1,9 +0,0 @@
-from __future__ import absolute_import
-from __future__ import division
-from __future__ import print_function
-
-from .ctdet import CtdetTrainer
-
-train_factory = {
-  'ctdet': CtdetTrainer
-}
